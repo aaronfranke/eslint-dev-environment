e2f16c72dd14c149e3d1b31e7ffe498a833657bf
Aaron Franke <arnfranke@yahoo.com>
2020-11-20 19:31:08 -0500

    Format repo using Prettier

--- main.js
+++ main.js
@@ -1,45 +1,45 @@
 const budget = {
 	myBudget: [
 		{
-			title: 'Phone',
-			budget: 35
+			title: "Phone",
+			budget: 35,
 		},
 		{
-			title: 'Internet',
-			budget: 45
+			title: "Internet",
+			budget: 45,
 		},
 		{
-			title: 'Education',
-			budget: 80
+			title: "Education",
+			budget: 80,
 		},
 		{
-			title: 'Eat out',
-			budget: 25
+			title: "Eat out",
+			budget: 25,
 		},
 		{
-			title: 'Rent',
-			budget: 275
+			title: "Rent",
+			budget: 275,
 		},
 		{
-			title: 'Grocery',
-			budget: 110
+			title: "Grocery",
+			budget: 110,
 		},
 		{
-			title: 'Pharmacy',
-			budget: 40
+			title: "Pharmacy",
+			budget: 40,
 		},
 		{
-			title: 'Donations',
-			budget: 10
+			title: "Donations",
+			budget: 10,
 		},
-	]
+	],
 };
 
 function generateTable(b) {
-	const p = document.getElementById('list');
-	p.innerHTML = '';
+	const p = document.getElementById("list");
+	p.innerHTML = "";
 	for (var i = 0; i < b.length; i++) {
-		const div = document.createElement('div');
+		const div = document.createElement("div");
 		div.innerHTML = `${b[i].title} - ${b[i].budget}`;
 		p.appendChild(div);
 	}
@@ -49,24 +49,24 @@ generateTable(budget.myBudget);
 
 function aZ() {
 	const newArray = [...budget.myBudget];
-	newArray.sort((a, b) => (a.title > b.title) ? 1 : - 1);
+	newArray.sort((a, b) => (a.title > b.title ? 1 : -1));
 	generateTable(newArray);
 }
 
 function zA() {
 	const newArray = [...budget.myBudget];
-	newArray.sort((a, b) => (a.title < b.title) ? 1 : - 1);
+	newArray.sort((a, b) => (a.title < b.title ? 1 : -1));
 	generateTable(newArray);
 }
 
 function biggerBudget() {
 	const newArray = [...budget.myBudget];
-	newArray.sort((a, b) => (a.budget < b.budget) ? 1 : - 1);
+	newArray.sort((a, b) => (a.budget < b.budget ? 1 : -1));
 	generateTable(newArray);
 }
 
 function smallerBudget() {
 	const newArray = [...budget.myBudget];
-	newArray.sort((a, b) => (a.budget > b.budget) ? 1 : - 1);
+	newArray.sort((a, b) => (a.budget > b.budget ? 1 : -1));
 	generateTable(newArray);
 }
